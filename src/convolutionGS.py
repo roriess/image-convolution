fc2543f9300b2fabc78b19cc7bf581a5b53226b0
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2026-03-15 11:31:14 +0300

    added color image conversion ot grayscale

--- src/convolutionGS.py
+++ src/convolutionGS.py
@@ -4,19 +4,21 @@ from kernels import *
 from padding import *
 import os
 
+def convert_to_GS(arr_img):
+    gray = 0.299 * arr_img[:,:,0] + 0.587 * arr_img[:,:,1] + 0.114 * arr_img[:,:,2] # коэффициенты luminosity
+    return gray.astype(np.uint8)
+
 def convolution_GS(image, kernel, padding):
     img = os.path.join("images", image)
     img = Image.open(img)
 
-    if img.format != "L": 
-        img = img.convert("L")
-
     arr_img = np.array(img)
+    if len(arr_img.shape) == 3: 
+        arr_img = convert_to_GS(arr_img)
 
     kernel_height, _ = kernel.shape
 
     new_img = add_padding(arr_img, kernel_height, padding)
-
     img_height, img_width = new_img.shape
 
     for i in range(img_height):
