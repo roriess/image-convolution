dc65ed4f9f8caf88384c48002383353f3faf7f0d
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2026-04-28 23:59:53 +0300

    replace argparse with typer in CLI

--- src/main.py
+++ src/main.py
@@ -1,50 +1,43 @@
 from src.convolution import convolution_grayscale, convolution_rgb
-import argparse
 from PIL import Image
 import numpy as np
+from src.benchmark.benchmark import run_benchmark
+import typer
+
+app = typer.Typer()
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Convolution image")
-
-    parser.add_argument("--input_dir", type=str, required=True, help="Input directory")
-    parser.add_argument(
-        "--output_dir", type=str, required=True, help="Output directory"
-    )
-    parser.add_argument(
-        "--image_mode",
-        type=str,
-        required=True,
-        help="Image color mode: 'L' for grayscale, 'RGB' for color",
-    )
-    parser.add_argument(
-        "--kernel", type=str, required=True, help="Selected filter for photo processing"
-    )
-    parser.add_argument(
-        "--padding", type=str, required=True, help="Border processing style"
-    )
-
-    args = parser.parse_args()
-
-    if args.image_mode != "L" and args.image_mode != "RGB":
+@app.command()
+def run(
+    input_dir: str = typer.Option(..., help="Input directory"),
+    output_dir: str = typer.Option(..., help="Output directory"),
+    image_mode: str = typer.Option(..., help="Image mode: 'L' or 'RGB'"),
+    kernel: str = typer.Option(..., help="Kernel name"),
+    padding: str = typer.Option(..., help="Padding type"),
+):
+    if image_mode != "L" and image_mode != "RGB":
         raise ValueError(
-            f"Unsupported image mode: {args['image_mode']}. Use 'L' for grayscale or 'RGB' for color."
+            f"Unsupported image mode: {image_mode}. Use 'L' for grayscale or 'RGB' for color."
         )
-
-    img = Image.open(args.input_dir).convert(args.image_mode)
+    img = Image.open(input_dir).convert(image_mode)
     arr_img = np.asarray(img, dtype=np.float32)
 
-    func = convolution_grayscale if args.image_mode == "L" else convolution_rgb
+    func = convolution_grayscale if image_mode == "L" else convolution_rgb
     new_img = func(
         arr_img=arr_img,
-        kernel=args.kernel,
-        padding=args.padding,
+        kernel=kernel,
+        padding=padding,
     )
 
     new_img = np.clip(new_img, 0, 255).astype(np.uint8)
     result = Image.fromarray(new_img)
-    result.save(args.output_dir)
+    result.save(output_dir)
+
+
+@app.command()
+def benchmark():
+    run_benchmark()
 
 
 if __name__ == "__main__":
-    main()
+    app()
